7db7f8dc1e243f3b380c0a2b77ca3c771be5bb9d
AngelDevHub <rvangel44gc@gmail.com>
2025-11-11 17:22:47 -0600

    Conexion firebase actualizada

--- src/firebase.js
+++ src/firebase.js
@@ -3,19 +3,22 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-// Railway o local: usa FIREBASE_CONFIG como variable JSON
-const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
+const serviceAccount = {
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://fir-adminsdk-ceaa-default-rtdb.firebaseio.com"
+  databaseURL: process.env.FIREBASE_DB_URL
 });
 
 console.log("🔥 Firebase inicializado correctamente");
 
 const db = admin.database();
-
 export { admin, db };
+
 
 
 /*import admin from "firebase-admin";
