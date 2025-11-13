d74782d96c2bf6b1198fc45992c0cfa7895cfd3b
AngelDevHub <rvangel44gc@gmail.com>
2025-11-13 14:22:45 -0600

    Cambios

--- src/app.js
+++ src/app.js
@@ -79,7 +79,7 @@ app.use(cors({
     credentials: true
 }));
 
-app.options('*', cors({
+app.use(cors({
     origin: allowedOrigins,
     credentials: true,
     methods: ['GET','POST','PUT','DELETE','PATCH','OPTIONS'],
